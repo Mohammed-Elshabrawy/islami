888b9f9ea500da287d771235d9b28671ea170d37
Mohammed-Elshabrawy <m.e18520029@gmail.com>
2026-04-14 17:59:54 +0200

    fix flashing light when navigate

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -3,19 +3,19 @@ import 'app_colors.dart';
 
 class AppTheme {
   static final ThemeData darkMode = ThemeData(
-      scaffoldBackgroundColor: AppColors.transparentColor,
-    appBarTheme: AppBarTheme(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColors.blackColor,
+    canvasColor: AppColors.blackColor,
+    appBarTheme: const AppBarTheme(
       centerTitle: true,
       titleTextStyle: TextStyle(
         color: AppColors.goldColor,
         fontSize: 20,
         fontWeight: FontWeight.bold,
       ),
-
       backgroundColor: AppColors.blackColor,
-      iconTheme: IconThemeData(
-        color: AppColors.goldColor
-      ),
+      surfaceTintColor: Colors.transparent,
+      iconTheme: IconThemeData(color: AppColors.goldColor),
     ),
   );
 }
